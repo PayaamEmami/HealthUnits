2183e437139cfce11a82104f23f360aa7d1d8c09
payaamemami <payaamemami@gmail.com>
2017-08-22 15:16:53 -0700

    Added primary lua file

---
+++ HealthUnits.lua
@@ -0,0 +1,53 @@
+-- [[ FormatNumber: formats the number argument and returns the formatted version (Ex: 6583984 -> 6.58M)]]
+local FormatNumber = function(num)
+    local convertedNumber
+
+    -- Checks empty variable
+    if not num then
+        -- Trillion
+        return 0
+    elseif num >= 1000000000000 then
+        -- Billion
+        convertedNumber = string.format("%.2f", (num / 1000000000000)) .. "T"
+    elseif num >= 1000000000 then
+        -- Million
+        convertedNumber = string.format("%.2f", (num / 1000000000)) .. "B"
+    elseif num >= 1000000 then
+        -- Thousand
+        convertedNumber = string.format("%.2f", (num / 1000000)) .. "M"
+    elseif num >= 1000 then
+        -- Hundreds
+        convertedNumber = string.format("%.2f", (num / 1000)) .. "k"
+    else
+        convertedNumber = num
+    end
+
+    return convertedNumber
+end
+
+-- [[ Updates the status text values to formatted numbers ]]
+-- NOTE: The following code has not yet been tested. Remove this comment after testing.
+hooksecurefunc(
+    "TextStatusBar_UpdateTextStringWithValues",
+    function()
+        -- Converts numbers and stores into variables
+        PlayerHealth = FormatNumber(UnitHealth("player"))
+        PlayerMana = FormatNumber(UnitMana("player"))
+
+        TargetHealth = FormatNumber(UnitHealth("target"))
+        TargetMana = FormatNumber(UnitMana("target"))
+
+        FocusHealth = FormatNumber(UnitHealth("focus"))
+        FocusMana = FormatNumber(UnitMana("focus"))
+
+        -- Updates the frames with formatted values
+        PlayerFrameHealthBar.TextString:SetText(PlayerHealth)
+        PlayerFrameManaBar.TextString:SetText(PlayerMana)
+
+        TargetFrameHealthBar.TextString:SetText(TargetHealth)
+        TargetFrameManaBar.TextString:SetText(TargetMana)
+
+        FocusFrameHealthBar.TextString:SetText(FocusHealth)
+        FocusFrameManaBar.TextString:SetText(FocusMana)
+    end
+)
